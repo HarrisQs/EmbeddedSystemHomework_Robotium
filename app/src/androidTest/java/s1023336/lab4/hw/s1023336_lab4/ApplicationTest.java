99f83796777ef68f9dc7fac89a0e28a7769ac158
HarrisQs <harris32916@gmail.com>
2015-12-02 20:06:12 +0800

    Test environment is building

--- app/src/androidTest/java/s1023336/lab4/hw/s1023336_lab4/ApplicationTest.java
+++ app/src/androidTest/java/s1023336/lab4/hw/s1023336_lab4/ApplicationTest.java
@@ -2,12 +2,35 @@ package s1023336.lab4.hw.s1023336_lab4;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
+import com.robotium.solo.Solo;
+import android.test.ActivityInstrumentationTestCase2;
 
-/**
- * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
- */
-public class ApplicationTest extends ApplicationTestCase<Application> {
+public class ApplicationTest extends ActivityInstrumentationTestCase2<MainActivity> {
     public ApplicationTest() {
-        super(Application.class);
+        super(MainActivity.class);
+    }
+    public void setUp() throws Exception
+    {
+
+    }
+    @Override
+    public void tearDown() throws Exception
+    {
+
+    }
+    public void testAddNote() throws Exception
+    {
+
+    }
+    public void testEditNote() throws Exception
+    {
+
+    }
+    public void setUp() throws Exception {
+        solo = new Solo(getInstrumentation(), getActivity());
+    }
+    @Override
+    public void tearDown() throws Exception {
+        solo.finishOpenedActivities();
     }
 }
