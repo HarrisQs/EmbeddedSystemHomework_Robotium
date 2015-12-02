8371a5951aa80c5c73da50fb87f4addd408528c7
HarrisQs <harris32916@gmail.com>
2015-12-02 18:43:30 +0800

    First initial

---
+++ app/src/androidTest/java/s1023336/lab4/hw/s1023336_lab4/ApplicationTest.java
@@ -0,0 +1,13 @@
+package s1023336.lab4.hw.s1023336_lab4;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ app/src/test/java/s1023336/lab4/hw/s1023336_lab4/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package s1023336.lab4.hw.s1023336_lab4;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
