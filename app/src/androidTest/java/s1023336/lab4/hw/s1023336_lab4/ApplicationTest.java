ce69bf67f601c1a9e42a7a7936eba2ee8ddd2424
HarrisQs <harris32916@gmail.com>
2015-12-03 12:40:11 +0800

    add to test add function code

--- app/src/androidTest/java/s1023336/lab4/hw/s1023336_lab4/ApplicationTest.java
+++ app/src/androidTest/java/s1023336/lab4/hw/s1023336_lab4/ApplicationTest.java
@@ -1,36 +1,35 @@
 package s1023336.lab4.hw.s1023336_lab4;
 
-import android.app.Application;
-import android.test.ApplicationTestCase;
 import com.robotium.solo.Solo;
 import android.test.ActivityInstrumentationTestCase2;
 
 public class ApplicationTest extends ActivityInstrumentationTestCase2<MainActivity> {
-    public ApplicationTest() {
+    private Solo solo;
+    public ApplicationTest()
+    {
         super(MainActivity.class);
-    }
-    public void setUp() throws Exception
-    {
-
-    }
-    @Override
-    public void tearDown() throws Exception
-    {
-
-    }
-    public void testAddNote() throws Exception
-    {
-
-    }
-    public void testEditNote() throws Exception
-    {
-
     }
     public void setUp() throws Exception {
         solo = new Solo(getInstrumentation(), getActivity());
     }
     @Override
     public void tearDown() throws Exception {
-        solo.finishOpenedActivities();
+        //solo.finishOpenedActivities();
+    }
+    public void testBasicAdd() throws Exception
+    {
+        solo.clickOnButton("4");
+        solo.clickOnButton("9");
+        solo.clickOnButton("2");
+        solo.clickOnButton("Space");
+        solo.clickOnButton("2");
+        solo.clickOnButton("8");
+        solo.clickOnButton("5");
+        solo.clickOnButton("Space");
+        solo.clickOnButton("+");
+        solo.clickOnButton("=");
+        boolean notesFound = solo.searchText("777");
+        assertTrue("Note 1 and/or Note 2 are not found", notesFound);
+
     }
 }
